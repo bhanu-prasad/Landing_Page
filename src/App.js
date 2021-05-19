277fc8c6988a217dba9d4fb39b22ebd50379fecd
Neelima-git <bellana.neelima2@gmail.com>
2021-05-20 01:26:36 +0530

    pricing page done with componentsgit add .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react"
 import ReactDom from "react-dom"
+import Price from "./components/price.jsx";
+// import Pricing from "./components/pricing.jsx";
 
 class App extends React.Component{
     render(){
-        return(<h1>Hello</h1>)
+        return(
+            <Price />
+        )
     }
 }
 export default App
