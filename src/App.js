fa1de2d040c08cd8df0ec1385cf1298e2d7d1073
unknown <bhanu.prasad1908@gmail.com>
2021-05-19 20:56:08 +0530

    home&services

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react"
 import ReactDom from "react-dom"
-
+import Home from "./components/home"
+import Services from "./components/services"
 class App extends React.Component{
     render(){
-        return(<h1>Hello</h1>)
+        return(<>
+        <Home/>
+        <Services/>
+        </>)
     }
 }
 export default App
