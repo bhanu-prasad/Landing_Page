b7f0647d899ebe3d7aa7ded398e4cbfddec7151f
Neelima-git <bellana.neelima2@gmail.com>
2021-05-20 14:30:40 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react"
 import ReactDom from "react-dom"
-import Price from "./components/price.jsx";
-// import Pricing from "./components/pricing.jsx";
+import Navbar from "./components/navbar"
+import Home from "./components/home"
+import Services from "./components/services"
+import Footer from "./components/footer"
+import Brands from "./components/brands"
+import Getintouch from "./components/getintouch"
+import "./App.css"
+import "./css/default.css"
+import "./css/style.css"
+import "./css/LineIcons.css"
+import "./css/style.css.map"   
+import "./css/magnific-popup.css"
+import ScrollToTop from "./components/scrolltotop"
 
 class App extends React.Component{
     render(){
-        return(
-            <Price />
-        )
+        return(<>
+        <Navbar/>
+        <Home/>
+        <Services/>
+        <Brands/>
+        <Getintouch/>
+        <Footer/>
+        <ScrollToTop/>
+        </>)
     }
 }
 export default App
---
+++ src/components/scrollToTop.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../css/default.css";
+import "../css/style.css";
+import "../css/LineIcons.css";
+import "../css/style.css.map";
+import "../css/magnific-popup.css";
+import "../css/pricing.css";
+
+const ScrollToTop = () => {
+    return (
+        <div>            
+            <a class="back-to-top" href="#">
+                <i class="lni-chevron-up"></i>
+            </a>
+        </div>
+    )
+}
+
+export default ScrollToTop
