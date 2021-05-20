c7da548665160069931d385523b2f7d40ed1ee17
bhanu-prasad <bhanu.prasad1908@gmail.com>
2021-05-20 17:03:46 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from "react"
 import ReactDom from "react-dom"
 import Navbar from "./components/navbar"
+import Home from "./components/home"
+import Services from "./components/services"
+import Footer from "./components/footer"
+import Brands from "./components/brands"
+import Getintouch from "./components/getintouch"
+import Testimonial from "./components/Testimonial"
+import Price from "./components/price"
+import Calltoaction from "./components/calltoaction"
+import ScrollToTop from "./components/scrolltotop"
 
 class App extends React.Component{
     render(){
-        return(
-            <div className='App'>
-                <Navbar/>
-
-            </div>
-        )
+        return(<>
+        <Navbar/>
+        <Home/>
+        <Services/>
+        <Price />
+        <Calltoaction/>
+        <Testimonial/>
+        <Brands/>
+        <Getintouch/>
+        <Footer/>
+        <ScrollToTop/>
+        
+        </>)
     }
 }
 export default App
